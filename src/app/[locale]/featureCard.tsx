866496631266795d7e322ca220d86252fd5e9c2d
Ashik-Himel <ashikujjamanhimel@gmail.com>
2025-03-22 15:01:09 +0600

    features section translation added

--- src/app/[locale]/featureCard.tsx
+++ src/app/[locale]/featureCard.tsx
@@ -7,7 +7,7 @@ export default function FeatureCard({
 }) {
   return (
     <div className="p-6 rounded-lg border border-gray-300 dark:border-[#5A2A2A] bg-white dark:bg-[#472020] text-center">
-      <span className="block text-3xl md:text-4xl lg:text-5xl font-bold text-gradient mb-4">
+      <span className="block text-4xl md:text-5xl font-bold text-gradient mb-2">
         {text}
       </span>
       <span className="text-gray-500 dark:text-[#ffefef]">{subtext}</span>
--- src/app/[locale]/featuresSection.tsx
+++ src/app/[locale]/featuresSection.tsx
@@ -1,16 +1,37 @@
+"use client";
+
+import { convertToBanglaNumber } from "@/lib/convertToBanglaNumber";
+import { useTranslations } from "next-intl";
+import { useParams } from "next/navigation";
 import FeatureCard from "./featureCard";
 
 export default function FeaturesSection() {
+  const params = useParams();
+  const t = useTranslations("HomePage.FeaturesSection");
+
   return (
     <section className="pb-12 md:pb-16 lg:pb-20">
-      <div className="container grid grid-cols-1 min-[400px]:grid-cols-2 lg:grid-cols-4 gap-4">
+      <div className="container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
         <FeatureCard
-          text={(new Date().getFullYear() - 2009).toString()}
-          subtext="Years Training Experience"
+          text={
+            params?.locale === "bn"
+              ? convertToBanglaNumber(new Date().getFullYear() - 2009)
+              : (new Date().getFullYear() - 2009).toString()
+          }
+          subtext={t("feature-1-subtext")}
         />
-        <FeatureCard text="120K" subtext="Students Trained" />
-        <FeatureCard text="95%" subtext="Student Satisfied" />
-        <FeatureCard text="24/7" subtext="Support" />
+        <FeatureCard
+          text={t("feature-2-text")}
+          subtext={t("feature-2-subtext")}
+        />
+        <FeatureCard
+          text={t("feature-3-text")}
+          subtext={t("feature-3-subtext")}
+        />
+        <FeatureCard
+          text={t("feature-4-text")}
+          subtext={t("feature-4-subtext")}
+        />
       </div>
     </section>
   );
